7551227fa22057c189482c84683cc8f3d31936e3
eliachow <eliacchow@gmail.com>
2023-05-07 14:40:11 -0700

    add more data to users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@example.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@example.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Sally', 'sally@example.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Jane', 'jane@example.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Brenda', 'brenda@example.com', 'password');
+
