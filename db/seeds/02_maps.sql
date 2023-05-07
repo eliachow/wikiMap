16e0fb56ff56d6f491c398c7a8e0ef164bfc9b9e
eliachow <eliacchow@gmail.com>
2023-05-07 14:30:12 -0700

    create maps data

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,8 @@
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Vancouver', '2023-01-01 12:00:00', 1, 49.2827, -123.1207);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Toronto', '2023-02-01 12:00:00', 1, 43.6532, -79.3832);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Calgary', '2023-03-01 12:00:00', 2, 51.0447, -114.0719);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Ottowa', '2023-02-01 12:00:00', 2, 45.4215, -75.6981);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Los Angeles', '2023-02-01 12:00:00', 3, 34.0522, -118.2437);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('New York', '2023-02-01 12:00:00', 3, 40.7128, -74.0060);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Halifax', '2023-02-01 12:00:00', 4, 44.6488, -63.5752);
+INSERT INTO maps (name, created_at, creator_id, lat, lon) VALUES ('Edmonton', '2023-02-01 12:00:00', 5, 53.5444, -113.4909);
