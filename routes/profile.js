5b1908b680a754a360fa46d0eadcdaa7e7eab9c7
eliachow <eliacchow@gmail.com>
2023-05-07 15:37:59 -0700

    create loop of fav_maps by userID

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const { getFavMapsByUser } = require('../db/queries/fav_maps');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('profile');
+// Render user's profile page
+router.get('/:id', (req, res) => {
+  const { id }= req.params;
+  /**option to use templateVars:
+   * const templateVars = {}
+   */
+  getFavMapsByUser(id)
+  .then(data => {
+    // send object list of fav maps to profile.ejs file by userID
+      res.render('profile', {favMaps: data.rows});
+  })
+  .catch(err => {
+      res
+        .status(500)
+        // need to create error ejs page - render error page: 
+        // .render('error', {
+        //   message: 'Something went wrong'
+        // });
+        console.log("error: ", err);
+     })
+
 }); 
 
 module.exports = router;
