83c2ead8f0bd0b8d31870b3ab8ec4b676c807c37
eliachow <eliacchow@gmail.com>
2023-05-07 14:43:52 -0700

    swith cookie parser to cookie sessions

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
 
 const PORT = process.env.PORT || 8080;
@@ -17,7 +17,12 @@ app.set('view engine', 'ejs');
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["user_id"],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   '/styles',
