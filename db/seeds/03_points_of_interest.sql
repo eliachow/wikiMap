7a07dffa8091088c702e97eb093038f1f2921bb0
eliachow <eliacchow@gmail.com>
2023-05-07 14:30:50 -0700

    create points_of_interest data

---
+++ db/seeds/03_points_of_interest.sql
@@ -0,0 +1,9 @@
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2822, -123.1785, 1);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Culinary Delights', 'Dessert Cafe', 'img.url', 49.2823, -123.1186, 1);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Emerald Meadow Park', 'Dog park', 'img.url', 49.2821, -123.1185, 2);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 3);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 4);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 5);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 6);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 7);
+INSERT INTO points_of_interest (title, description, image_url, lat, lon, map_id) VALUES ('Savoria Bistro', 'Italian Restaurant', 'img.url', 49.2821, -123.1185, 8);
