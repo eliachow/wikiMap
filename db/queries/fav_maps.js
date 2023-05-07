e4280f6a3b810e23b8559f57c221a4231d3c44cb
eliachow <eliacchow@gmail.com>
2023-05-07 15:27:38 -0700

    fix syntax error, change favorite_maps to fav_maps

--- db/queries/fav_maps.js
+++ db/queries/fav_maps.js
@@ -1,5 +1,11 @@
 const db = require('../connection');
 
+// 1. Query for read favorite_maps by user route
+const getFavMapsByUser = (userID) => {
+  return db.query(`SELECT DISTINCT fav_maps.*, maps.* 
+  FROM maps 
+  JOIN fav_maps ON map_id = maps.id 
+  WHERE fav_maps.user_id = $1`, [userID])  
+}
 
-
-module.exports = {  };
+module.exports = { getFavMapsByUser };
