88075ca208c63d0921d3a9bb198b528cbf03bc04
eliachow <eliacchow@gmail.com>
2023-05-07 18:45:03 -0700

    refactor get index to call getAllMaps and send object data to index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const { getAllMaps } = require('../db/queries/maps');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  getAllMaps()
+  .then(results => {
+      res.render('index', {maps: results.rows});
+  })
+  .catch(err => {
+    res
+      .status(500)
+      console.log("error: ", err);
+  })
+
 }); 
 
 module.exports = router;
