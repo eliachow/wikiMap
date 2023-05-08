ef6e9e183e4d4e95145d0bf833ce9d65907ce3ee
eliachow <eliacchow@gmail.com>
2023-05-07 19:22:48 -0700

    create fav_maps query to add map to fav_maps table

--- db/queries/fav_maps.js
+++ db/queries/fav_maps.js
@@ -9,6 +9,12 @@ const getFavMapsByUser = (userID) => {
 }
 
 
+// Add map to fav_map list (on index page)
+const addFavMaps = (mapID, userID) => {
+  return db.query(`INSERT INTO fav_maps (map_id, user_id) VALUES ($1, $2);`, [mapID, userID])
+}
+
+
 // Delete from fav_maps list (on profile page)
 const removeFavMaps = (mapID, userID) => {
   return db.query(`DELETE FROM fav_maps WHERE map_id = $1 AND user_id = $2;`, [mapID, userID])     
@@ -17,5 +23,6 @@ const removeFavMaps = (mapID, userID) => {
 
 module.exports = { 
   getFavMapsByUser,
-  removeFavMaps
+  removeFavMaps,
+  addFavMaps
  };
